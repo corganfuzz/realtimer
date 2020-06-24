1bb930c6f339a5611499d7942818896be1441f72
corganfuzz <gruizcavero@uh.edu>
2020-06-24 17:07:39 -0500

    db connected to atlas

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected!"))
+  .catch((err) => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
